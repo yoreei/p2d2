862ea205ca7d5bf21da36581397dda611e15a088
Yordan Grigorov <yoreei.grigorov@gmail.com>
2021-06-30 13:47:22 +0200

    prepared module4 for benchmark 1/2

--- benchmarks-kaggle/base/module4.py
+++ benchmarks-kaggle/base/module4.py
@@ -1,66 +1,231 @@
-#!/bin/env python3
-import pandas as pd
-import numpy as np
-import psycopg2
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 import time
+import helppd2sql
+import psycopg2
+
+
+# In[2]:
 
 start_clock = time.perf_counter()
-conn = psycopg2.connect(CONNSTR)
-# variable CONN should be provided by the overseeing script. See benchmarker/main.py
-sentiment_values_df = pd.read_sql_query("SELECT * FROM sentiment_values", conn)
-user_track_df = pd.read_sql_query("SELECT * FROM user_track", conn)
-content_features_df = pd.read_sql_query("SELECT * FROM content_features", conn)
+conn = psycopg2.connect(f"host=localhost dbname=module4 user=disable_nestloop_user password=disable_nestloop_user")
 
-#SHARED_DB_TIME is multiprocessing.Value
-SHARED_DB_TIME.value = time.perf_counter() - start_clock
+sentiment_values = pandas.read_sql_query('select * from sentiment_values', con=conn)
+user_track = pandas.read_sql_query('select * from user_track', con=conn)
+context_content_features = pandas.read_sql_query('select * from context_content_features', con=conn)
 
-sentiment_values_df = sentiment_values_df.drop(columns=['vader_min','vader_max','vader_sum','afinn_min','afinn_max','afinn_sum','ol_min','ol_max','ol_sum','ss_min','ss_max','ss_sum', 'vader_score','afinn_score','ss_score','vader_avg','afinn_avg','ol_avg','ss_avg'])
+calc_time = time.perf_counter() - start_clock
+print(f"db_time: {calc_time}")
 
-#Fill in missing ol_score with ol_avg score, if available
-sentiment_values_df['ol_score'] = sentiment_values_df.apply(
-    lambda row: row['ol_avg'] if np.isnan(row['ol_score']) else row['ol_score'],
-    axis=1
-)
+start_clock = time.perf_counter()
 
-sentiment_values_df = sentiment_values_df.dropna()
+# In[3]:
 
-print (sentiment_values_df.nlargest(10, 'ol_score'))
 
-user_track_df = user_track_df.dropna()
+proj_sent = sentiment_values[['hashtag', 'ol_score', 'ol_avg']]
 
-# Select the items where the track_id count is less than 50 and remove them
-counts = user_track['track_id'].value_counts()
-user_track = user_track[~user_track['track_id'].isin(counts[counts < 50].index)]
 
-sentiment_df = sentiment_values.merge(user_track, on='hashtag')
+# In[4]:
 
-# Select the items where the track_id count is less than 50 and remove them
-counts = content_features['track_id'].value_counts()
-content_features_df = content_features_df[~content_features_df['track_id'].isin(counts[counts < 50].index)]
 
-content_features_df = content_features.drop(columns=['coordinates','id','place','geo'])
-content_features_df = content_features_df.dropna()
-content_features_df = content_features_df[content_features_df['lang'] == 'en']
-joined = sentiment_df.merge(content_features_df, on=['track_id','created_at','user_id'])
-joined['sentiment'] = np.where(df4['sentiment_score']>= 0.01, 1, 0)
-joined = df4.dropna()
+# proj_sent.apply(lambda x: x.isnull().sum())
 
-joined = joined.drop(columns=['hashtag','created_at','artist_id','tweet_lang','lang'])
-joined['time_zone'].replace('Eastern Time (US & Canada)', 'Eastern Time',inplace=True)
-joined['time_zone'].replace('Central Time (US & Canada)', 'Central Time',inplace=True)
-joined['time_zone'].replace('Pacific Time (US & Canada)', 'Pacific Time',inplace=True)
-joined['time_zone'].replace('Mountain Time (US & Canada)', 'Mountain Time',inplace=True)
-joined['time_zone'].replace('Alaska', 'Alaska Time',inplace=True)
-joined['time_zone'].replace('Hawaii', 'Hawaii Time',inplace=True)
-joined['time_zone'].replace('Arizona', 'Mountain Time',inplace=True)
-joined['time_zone'].replace('America/Chicago', 'Central Time',inplace=True)
-joined['time_zone'].replace('America/New_York', 'Eastern Time',inplace=True)
-joined['time_zone'].replace('America/Los_Angeles', 'Pacific Time',inplace=True)
-joined['time_zone'].replace('America/Denver', 'Mountain Time',inplace=True)
-joined['time_zone'].replace('America/Detroit', 'Eastern Time',inplace=True)
 
-joined = joined[joined['time_zone'].isin(['Eastern Time','Central Time','Pacific Time','Mountain Time','Alaska Time','Hawaii Time']
+# In[5]:
 
-# ML
 
-action(joined)
+# COALESCE (ol_score, ol_avg) NOT SUPPORTED YET
+proj_sent['ol_score'] = proj_sent['ol_score'].combine_first(
+    proj_sent['ol_avg'])
+
+
+# In[6]:
+
+
+# proj_sent.apply(lambda x: x.isnull().sum())
+
+
+# In[7]:
+
+
+# UNREACHABLE
+dropna_sentiments = proj_sent.dropna(axis = 0, how ='any')
+
+
+# In[8]:
+
+
+# UNREACHABLE
+dropna_sentiments.rename(columns = {'ol_score':'sentiment_score'}, inplace = True)
+
+
+# In[9]:
+
+
+
+
+# In[10]:
+
+
+# explorative analysis - comment out for benchmark
+# df2.apply(lambda x: x.isnull().sum())
+
+
+# In[11]:
+
+
+dropna_user = user_track.dropna(subset=['hashtag'])
+
+
+# In[12]:
+
+
+counts = dropna_user['track_id'].value_counts()
+popular = counts[counts >= 50]
+pop_user_list = popular.index
+
+
+# In[13]:
+
+
+pop_user = dropna_user[dropna_user['track_id'].
+                                       isin(pop_user_list)]
+
+
+# In[36]:
+
+
+
+
+# In[65]:
+
+
+counts3 = context_content_features['track_id'].value_counts()
+popular3 = counts3[counts3 >= 50]
+pop_context_list = popular3.index
+
+
+# In[66]:
+
+
+print(len(context_content_features))
+print(len(pop_context_list))
+print(len(counts3))
+
+
+# In[67]:
+
+
+pop_context = context_content_features[context_content_features['track_id'].
+                                       isin(pop_context_list)]
+
+
+# In[68]:
+
+
+len(pop_context)
+
+
+# In[17]:
+
+
+# set(pop_context.columns) - {'coordinates','id',
+#    'place','geo', 'created_at','artist_id','tweet_lang'}
+
+
+# In[18]:
+
+
+proj_context = pop_context.drop(
+    columns=['coordinates','id','place','geo','artist_id','tweet_lang'])
+helppd2sql.drop2select(proj_context)
+
+
+# In[19]:
+
+
+# proj_context.apply(lambda x: x.isnull().sum())
+
+
+# In[20]:
+
+
+dropna_context = proj_context.dropna(subset=
+                 ['instrumentalness', 'liveness', 'speechiness',
+                  'danceability', 'valence', 'acousticness',
+                  'energy', 'mode', 'key', 'time_zone', 'user_id'])
+
+
+# Reduced the dataset from 10,887,911 to **6,413,576** by dropping all null value rows.
+
+# In[21]:
+
+
+english_context = dropna_context[dropna_context['lang'] == 'en']
+
+
+# In[22]:
+
+
+# CTE rename_context
+english_context = english_context.drop(columns=['lang'])
+english_context['time_zone'].replace('Eastern Time (US & Canada)', 'Eastern Time', inplace=True)
+english_context['time_zone'].replace('Central Time (US & Canada)', 'Central Time', inplace=True)
+english_context['time_zone'].replace('Pacific Time (US & Canada)', 'Pacific Time', inplace=True)
+english_context['time_zone'].replace('Mountain Time (US & Canada)', 'Mountain Time', inplace=True)
+english_context['time_zone'].replace('Alaska', 'Alaska Time', inplace=True)
+english_context['time_zone'].replace('Hawaii', 'Hawaii Time', inplace=True)
+english_context['time_zone'].replace('Arizona', 'Mountain Time', inplace=True)
+english_context['time_zone'].replace('America/Chicago', 'Central Time', inplace=True)
+english_context['time_zone'].replace('America/New_York', 'Eastern Time', inplace=True)
+english_context['time_zone'].replace('America/Los_Angeles', 'Pacific Time', inplace=True)
+english_context['time_zone'].replace('America/Denver', 'Mountain Time', inplace=True)
+english_context['time_zone'].replace('America/Detroit', 'Eastern Time', inplace=True)
+helppd2sql.drop2select(english_context)
+
+
+# In[23]:
+
+
+usa_context = english_context[english_context['time_zone'].isin(
+    ['Eastern Time', 'Central Time', 'Pacific Time',
+     'Mountain Time', 'Alaska Time', 'Hawaii Time'])]
+
+
+# In[24]:
+
+
+df23 = usa_context.merge(
+    pop_user, on=['track_id','created_at','user_id'], how='inner')
+
+
+# In[25]:
+
+
+df23_drop = df23.drop(columns=['created_at'])
+helppd2sql.drop2select(df23_drop)
+
+
+# In[ ]:
+
+calc_time = time.perf_counter() - start_clock
+print(f"python_time: {calc_time}")
+
+env = globals().copy()
+report = helppd2sql.global_report(env)
+report_df = pandas.DataFrame(report)
+report_df.to_csv('pandas_report.csv', index=False)
+df23_drop.to_csv('df23_drop_p.csv', index=False)
+
--- benchmarks-kaggle/optimized/module4.py
+++ benchmarks-kaggle/optimized/module4.py
@@ -1,170 +1,96 @@
-#!/bin/env python3
-import pandas as pd
-import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import helppd2sql
+import pandas
 import psycopg2
 import time
 
-start_clock = time.perf_counter()
-conn = psycopg2.connect(f"host=localhost dbname=module4 user=root password=root")
-# variable CONN should be provided by the overseeing script. See benchmarker/main.py
-
-### Tables and their columns:
-
-# # context_content_features
-# coordinates     
-# instrumentalness
-# liveness        
-# speechiness     
-# danceability    
-# valence         
-# loudness        
-# tempo           
-# acousticness    
-# energy          
-# mode            
-# key             
-# artist_id       
-# place           
-# geo             
-# tweet_lang      
-# track_id        
-# created_at      
-# lang            
-# time_zone       
-# user_id         
-# id
-
-# # sentiment_values
-# hashtag    
-# vader_score
-# afinn_score
-# ol_score   
-# ss_score   
-# vader_min  
-# vader_max  
-# vader_sum  
-# vader_avg  
-# afinn_min  
-# afinn_max  
-# afinn_sum  
-# afinn_avg  
-# ol_min     
-# ol_max     
-# ol_sum     
-# ol_avg     
-# ss_min     
-# ss_max     
-# ss_sum     
-# ss_avg
-
-# # user_track
-# user_id   
-# track_id  
-# hashtag   
-# created_at
-# CTEs
-
-query = """
-WITH projected_sentiments AS (
-    SELECT hashtag, COALESCE(ol_score, ol_avg) AS ol_score
-    FROM sentiment_values
-), dropna_sentiments AS (
-    SELECT hashtag, ol_score FROM projected_sentiments
-    WHERE hashtag IS NOT NULL AND ol_score IS NOT NULL
-), renamed_sentiments AS(
-    SELECT hashtag, ol_score AS sentiment_score
-    FROM dropna_sentiments
-), nlargest_sentiments AS (
-    SELECT * FROM renamed_sentiments
-    ORDER BY sentiment_score DESC
-    LIMIT 10
+conn = psycopg2.connect(f"host=localhost dbname=module4 user=disable_nestloop_user password=disable_nestloop_user")
 
 
-), dropna_user_track AS (
+proj_sent_query = """
+    SELECT hashtag, ol_score, ol_avg
+    FROM sentiment_values;
+"""
+
+cte_statements = """
+WITH 
+dropna_user AS (
     SELECT * FROM user_track
     WHERE hashtag IS NOT NULL
-), listof_popular_tracks_user_tracks AS (
-    SELECT track_id FROM dropna_user_track
+), 
+counts AS (
+    SELECT track_id, count(*) AS count_col FROM dropna_user
     GROUP BY track_id
-    HAVING COUNT(track_id) > 50
-),  popular_user_track AS (
-    SELECT * FROM user_track
-    WHERE track_id IN (SELECT * FROM listof_popular_tracks_user_tracks)
-), sentiment_tracks AS (
-    SELECT * FROM popular_user_track
-    INNER JOIN renamed_sentiments 
-    ON popular_user_track.hashtag = renamed_sentiments.hashtag
+),
+popular AS (
+    SELECT track_id, count_col FROM counts
+    WHERE count_col >= 50
+),
+pop_user_list AS (
+    SELECT track_id FROM popular
+),
+pop_user AS (
+    SELECT * FROM dropna_user
+    WHERE track_id IN (SELECT * FROM pop_user_list)
+),
 
 
-), listof_popular_tracks_content_features AS (
-    SELECT track_id FROM context_content_features
+counts3 AS (
+    SELECT track_id, count(*) AS count_col FROM context_content_features
     GROUP BY track_id
-    HAVING COUNT(track_id) > 50
-),  popular_content_features AS (
+),
+popular3 AS (
+    SELECT track_id, count_col FROM counts3
+    WHERE count_col >= 50
+),
+pop_context_list AS (
+    SELECT track_id FROM popular3
+),
+pop_context AS (
     SELECT * FROM context_content_features
-    WHERE track_id IN (SELECT * FROM listof_popular_tracks_content_features)
-), projected_content_features AS (
-    SELECT instrumentalness, liveness, speechiness, danceability, valence, loudness, tempo, acousticness, energy, "mode", "key", artist_id, tweet_lang, track_id, created_at, lang, time_zone, user_id
-    FROM popular_content_features
-), dropna_content_features AS (
-    SELECT * FROM projected_content_features
-    WHERE instrumentalness IS NOT NULL AND
-          liveness IS NOT NULL AND
-          speechiness IS NOT NULL AND
-          danceability IS NOT NULL AND
-          valence IS NOT NULL AND
-          loudness IS NOT NULL AND
-          tempo IS NOT NULL AND
-          acousticness IS NOT NULL AND
-          energy IS NOT NULL AND
-          "mode" IS NOT NULL AND
-          "key" IS NOT NULL AND
-          artist_id IS NOT NULL AND
-          tweet_lang IS NOT NULL AND
-          track_id IS NOT NULL AND
-          created_at IS NOT NULL AND
-          lang IS NOT NULL AND
-          time_zone IS NOT NULL AND
-          user_id IS NOT NULL
-), english_content_features AS (
-    SELECT * FROM dropna_content_features
-    WHERE lang = 'en'
-), joined AS (
-    SELECT * FROM english_content_features
-    INNER JOIN sentiment_tracks
-    USING (track_id, created_at, user_id)
-), case_joined AS ( 
--- creating sentiment column while dropping:
--- hashtag, created_at, artist_id, tweet_lang, lang
-    SELECT CASE
-        WHEN sentiment_score >= 0.01 THEN 1
-        ELSE 0
-    END AS sentiment,
-    sentiment_score, user_id, track_id, instrumentalness, liveness, speechiness, danceability, valence, loudness, tempo, acousticness, energy, "mode", "key", time_zone
-    FROM joined
-), dropna_joined AS (
-    SELECT * FROM case_joined
-    WHERE
-        sentiment IS NOT NULL AND
-        sentiment_score IS NOT NULL AND
-        user_id IS NOT NULL AND
-        track_id IS NOT NULL AND
-        instrumentalness IS NOT NULL AND
-        liveness IS NOT NULL AND
-        speechiness IS NOT NULL AND
-        danceability IS NOT NULL AND
-        valence IS NOT NULL AND
-        loudness IS NOT NULL AND
-        tempo IS NOT NULL AND
-        acousticness IS NOT NULL AND
-        energy IS NOT NULL AND
+    WHERE track_id IN (SELECT * FROM pop_context_list)
+), 
+proj_context AS (
+    SELECT -- drop 'coordinates','id','place','geo','artist_id','tweet_lang'
+        "instrumentalness", "liveness", "speechiness", 
+        "danceability", "valence", "loudness", 
+        "tempo", "acousticness", "energy", 
+        "mode", "key", "track_id", 
+        "created_at", "lang", "time_zone", 
+        "user_id"
+    FROM pop_context
+), 
+dropna_context AS (
+    SELECT * FROM proj_context
+    WHERE 
+        "instrumentalness" IS NOT NULL AND
+        "liveness" IS NOT NULL AND
+        "speechiness" IS NOT NULL AND
+        "danceability" IS NOT NULL AND
+        "valence" IS NOT NULL AND
+        "acousticness" IS NOT NULL AND
+        "energy" IS NOT NULL AND
         "mode" IS NOT NULL AND
         "key" IS NOT NULL AND
-        time_zone IS NOT NULL
- 
-), df_mvp AS (
-    SELECT
-    sentiment,sentiment_score,user_id,track_id,
+        "time_zone" IS NOT NULL AND
+        "user_id" IS NOT NULL
+), 
+english_context AS (
+    SELECT * from dropna_context
+    WHERE lang = 'en'
+),
+rename_content_features AS (
+    SELECT -- drop "lang"
+        "instrumentalness", "liveness", "speechiness",
+        "danceability", "valence", "loudness",
+        "tempo", "acousticness", "energy",
+        "mode", "key", "track_id",
+        "created_at", "user_id",
     CASE
         WHEN time_zone = 'Eastern Time (US & Canada)' THEN 'Eastern Time'
         WHEN time_zone = 'Eastern Time (US & Canada)' THEN 'Eastern Time'
@@ -179,23 +105,37 @@ WITH projected_sentiments AS (
         WHEN time_zone = 'America/Los_Angeles' THEN 'Pacific Time'
         WHEN time_zone = 'America/Denver' THEN 'Mountain Time'
         WHEN time_zone = 'America/Detroit' THEN 'Eastern Time'
-    END AS time_zone,
-    instrumentalness, liveness,speechiness,danceability,valence,loudness,tempo,acousticness,energy,"mode","key"
-    FROM dropna_joined 
-    -- WHERE time_zone IN ('Eastern Time', 'Central Time', 'Pacific Time', 'Mountain Time', 'Alaska Time', 'Hawaii Time')
-  )
-
--- We could also fork timezone_fixes_join and drop 
---['user_id','track_id','sentiment','sentiment_score','instrumentalness','liveness','speechiness', 'danceability','valence','loudness','tempo','acousticness','energy','mode','key']
--- but that would increase network cost
-
-
-SELECT * FROM df_mvp;
+    END AS time_zone
+    FROM english_context
+),
+usa_context AS (
+    SELECT * FROM rename_content_features
+    WHERE time_zone IN ('Eastern Time', 'Central Time', 'Pacific Time', 'Mountain Time', 'Alaska Time', 'Hawaii Time')
+), 
+df23 AS (
+    SELECT * FROM usa_context
+    INNER JOIN pop_user
+    USING (track_id, created_at, user_id)
+),
+df23_drop AS(
+    SELECT -- drop created_at
+        "instrumentalness", "liveness", "speechiness", 
+        "danceability", "valence", "loudness", 
+        "tempo", "acousticness", "energy", 
+        "mode", "key", "track_id", 
+        "time_zone", "user_id", "hashtag"
+    FROM df23
+)
 """
-
-df_mvp = pd.read_sql_query(query, con=conn)
-#SHARED_DB_TIME is multiprocessing.Value
-SHARED_DB_TIME.value = time.perf_counter() - start_clock
-df_mvp.to_csv('df_mvp_sql.csv', index=False)
-# ML
+report = helppd2sql.new_report()
+for cte in ['dropna_user', 'counts', 'popular', 'pop_user_list', 'pop_user', 'counts3', 'popular3', 'pop_context_list', 'pop_context', 'proj_context', 'dropna_context', 'english_context', 'rename_content_features', 'usa_context', 'df23', 'df23_drop']:
+    cte_query = cte_statements + "select * from " + cte + ";"
+    start_clock = time.perf_counter()
+    df = pandas.read_sql_query(cte_query, con=conn)
+    helppd2sql.report_add(report, cte, df)
+    calc_time = time.perf_counter() - start_clock
+    print(f"{cte} {calc_time}")
+report_df = pandas.DataFrame(report)
+report_df.to_csv('sql_report.csv', index=False)
+df.to_csv('df23_drop_s.csv', index=False)
 
