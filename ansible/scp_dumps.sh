2f6cdcdb177052b37ea555ce760bfd9ab9a303e6
Yordan Grigorov <yoreei.grigorov@gmail.com>
2021-07-05 11:22:15 +0200

    scp dumps

---
+++ ansible/scp_dumps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scp ubuntu@ec2-18-190-157-154.us-east-2.compute.amazonaws.com:~/module4.dump ~/
+scp ubuntu@ec2-18-190-157-154.us-east-2.compute.amazonaws.com:~/tpch1.dump ~/
+scp ubuntu@ec2-18-190-157-154.us-east-2.compute.amazonaws.com:~/tpch10.dump ~/
+scp ubuntu@ec2-18-190-157-154.us-east-2.compute.amazonaws.com:~/tpch100.dump ~/
+
