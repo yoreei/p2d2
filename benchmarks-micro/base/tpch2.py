c2198890926ed1cb95f80002cdfcbc3293411da7
Yordan Grigorov <yoreei.grigorov@gmail.com>
2021-07-09 23:58:12 +0200

    add tpch2 and 3 to benches

---
+++ benchmarks-micro/base/tpch2.py
@@ -0,0 +1,45 @@
+import time
+start_clock = time.perf_counter()
+import pandas as pd
+import numpy as np
+
+### DEBUG
+# CONNSTR='postgresql://root:root@localhost/tpch1'
+# from types import SimpleNamespace
+# SHARED_DB_TIME=SimpleNamespace()
+### END DEBUG
+
+def udf_disc_price(extended, discount):
+    import numpy as np
+	return np.multiply(extended, np.subtract(1, discount))
+
+def udf_charge(extended, discount, tax):
+    import numpy as np
+	return np.multiply(extended, np.multiply(np.subtract(1, discount), np.add(1, tax)))
+
+    
+# variable CONNSTR should be provided by the overseeing script. See benchmarker/main.py
+
+region = pd.read_sql("SELECT * FROM region", con=CONNSTR)
+nation = pd.read_sql("SELECT * FROM nation", con=CONNSTR)
+supplier = pd.read_sql("SELECT * FROM supplier", con=CONNSTR)
+part = pd.read_sql("SELECT * FROM part", con=CONNSTR)
+partsupp = pd.read_sql("SELECT * FROM partsupp", con=CONNSTR)
+
+#SHARED_DB_TIME is multiprocessing.Value
+SHARED_DB_TIME.value = time.perf_counter() - start_clock
+
+part = part.astype({'p_container' : 'category'})
+
+ps = partsupp[["ps_partkey", "ps_suppkey", "ps_supplycost"]]
+p = part[["p_partkey", "p_mfgr", "p_size", "p_type"]][(part.p_size == 38) & (part.p_type.str.match(".*TIN$"))][["p_partkey", "p_mfgr"]]
+psp = ps.merge(p, left_on="ps_partkey", right_on="p_partkey")
+s = supplier[["s_suppkey", "s_nationkey", "s_acctbal", "s_name", "s_address", "s_phone", "s_comment"]]
+psps = psp.merge(s, left_on="ps_suppkey", right_on="s_suppkey")[["ps_partkey", "ps_supplycost", "p_mfgr", "s_nationkey",         "s_acctbal", "s_name", "s_address", "s_phone", "s_comment"]]
+nr = nation.merge(region[region.r_name == "MIDDLE EAST"], left_on="n_regionkey", right_on="r_regionkey")[["n_nationkey", "n_name"]]
+pspsnr = psps.merge(nr, left_on="s_nationkey", right_on="n_nationkey")[["ps_partkey", "ps_supplycost", "p_mfgr", "n_name", "s_acctbal", "s_name", "s_address", "s_phone", "s_comment"]]
+aggr = pspsnr.groupby("ps_partkey").agg({'ps_supplycost' : min}).reset_index()
+sj = pspsnr.merge(aggr, left_on=["ps_partkey", "ps_supplycost"], right_on=["ps_partkey", "ps_supplycost"])
+result = sj[["s_acctbal", "s_name", "n_name", "ps_partkey", "p_mfgr", "s_address", "s_phone", "s_comment"]].sort_values(["s_acctbal", "n_name", "s_name", "ps_partkey"], ascending=[False, True, True, True]).head(100)
+
+SHARED_WALL_TIME.value = time.perf_counter() - start_clock
---
+++ benchmarks-micro/base/tpch3.py
@@ -0,0 +1,41 @@
+import time
+start_clock = time.perf_counter()
+import pandas as pd
+import numpy as np
+    
+### DEBUG:
+# CONNSTR='postgresql://root:root@localhost/tpch1'
+# from types import SimpleNamespace
+# SHARED_DB_TIME=SimpleNamespace()
+### END DEBUG
+
+
+customer = pd.read_sql("SELECT * FROM customer", con=CONNSTR)
+orders = pd.read_sql("SELECT * FROM orders", parse_dates=['o_orderdate'], con=CONNSTR)
+lineitem = pd.read_sql("SELECT * FROM lineitem", parse_dates = ['l_shipdate', 'l_commitdate', 'l_receiptdate'], con=CONNSTR)
+print("finished downloading")
+#SHARED_DB_TIME is multiprocessing.Value
+SHARED_DB_TIME.value = time.perf_counter() - start_clock
+
+customer = customer.astype({'c_mktsegment': 'category'})
+orders = orders.astype({'o_orderstatus' : 'category', 'o_orderpriority' : 'category'})
+lineitem = lineitem.astype({'l_returnflag': 'category', 'l_linestatus': 'category'})
+
+
+def udf_disc_price(extended, discount):
+    import numpy as np
+	return np.multiply(extended, np.subtract(1, discount))
+
+def udf_charge(extended, discount, tax):
+    import numpy as np
+	return np.multiply(extended, np.multiply(np.subtract(1, discount), np.add(1, tax)))
+
+o  = orders[["o_orderkey", "o_custkey", "o_orderdate", "o_shippriority"]][orders.o_orderdate < "1995-03-29"][["o_orderkey", "o_custkey", "o_orderdate", "o_shippriority"]]
+c  = customer[["c_custkey", "c_mktsegment"]][customer.c_mktsegment == "FURNITURE"][["c_custkey", "c_mktsegment"]]
+oc = o.merge(c, left_on="o_custkey", right_on="c_custkey")[["o_orderkey", "o_orderdate", "o_shippriority"]]
+l = lineitem[["l_orderkey", "l_extendedprice", "l_discount", "l_shipdate"]][lineitem.l_shipdate > "1995-03-29"][["l_orderkey", "l_extendedprice", "l_discount"]]
+loc = l.merge(oc, left_on="l_orderkey", right_on="o_orderkey")
+loc["volume"] = loc.l_extendedprice * (1 - loc.l_discount)
+result = loc.groupby(["l_orderkey", "o_orderdate", "o_shippriority"]).agg({'volume' : sum}).reset_index()[["l_orderkey", "volume", "o_orderdate", "o_shippriority"]].sort_values(["volume", "o_orderdate"], ascending=[False, True]).head(10)
+
+SHARED_WALL_TIME.value = time.perf_counter() - start_clock
